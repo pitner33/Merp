abc38a8cda2d613df6bb5e569a6cb612c09646f7
Peter Kovacs <peter.kovacs@dpc.hu>
2019-06-18 11:57:50 +0200

    (add) D100 roll open/closed with test

---
+++ src/main/java/DiceRoll/D100Roll.java
@@ -0,0 +1,66 @@
+package DiceRoll;
+
+import java.util.Random;
+
+public class D100Roll {
+
+    public Integer d100Random() {
+        return new Random().nextInt(101);
+    }
+
+    public Integer d100RandomOpen() {
+        Integer d100 = d100Random();
+        Integer d100Open = d100;
+
+        while (d100 > 95) {
+            Integer d100Again = d100Random();
+            d100Open += d100Again;
+            d100 = d100Again;
+        }
+
+        if (d100 < 6) {
+            Integer d100Again = d100Random();
+            Integer d100Failure = d100Again;
+
+            while (d100Again > 95) {
+                Integer d100FailureAgain = d100Random();
+                d100Failure += d100FailureAgain;
+                d100Again = d100FailureAgain;
+            }
+            d100Open = d100 - d100Failure;
+        }
+
+        return d100Open;
+    }
+
+    public Integer d100FromRoll(String roll1, String roll2, String roll3, String roll4) {
+        Integer roll1Num = Integer.parseInt(roll1);
+        Integer roll2Num = Integer.parseInt(roll2);
+        Integer roll3Num = Integer.parseInt(roll3);
+        Integer roll4Num = Integer.parseInt(roll4);
+
+        Integer d100OpenFromRoll = roll1Num;
+
+        if (roll1Num < 6) {
+            d100OpenFromRoll -= roll2Num;
+            if (roll2Num > 95) {
+                d100OpenFromRoll -= roll3Num;
+                if (roll3Num > 95) {
+                    d100OpenFromRoll -= roll4Num;
+                }
+            }
+        }
+
+        if (roll1Num > 95) {
+            d100OpenFromRoll += roll2Num;
+            if (roll2Num > 95) {
+                d100OpenFromRoll += roll3Num;
+                if (roll3Num > 95) {
+                    d100OpenFromRoll += roll4Num;
+                }
+            }
+        }
+
+        return  d100OpenFromRoll;
+    }
+}
--- src/main/java/com/sol/merp/MerpApplication.java
+++ src/main/java/com/sol/merp/MerpApplication.java
@@ -1,5 +1,6 @@
 package com.sol.merp;
 
+import DiceRoll.D100Roll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,27 @@ public class MerpApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MerpApplication.class, args);
+
+        for (int i = 0; i < 25; i++) {
+            System.out.print(new D100Roll().d100Random()  + ", ");
+        }
+        System.out.println("");
+
+        for (int i = 0; i < 25; i++) {
+            System.out.print(new D100Roll().d100RandomOpen() + ", ");
+        }
+        System.out.println("");
+
+        System.out.println(new D100Roll().d100FromRoll("10", "10", "10", "10")); //10
+        System.out.println(new D100Roll().d100FromRoll("96", "10", "10", "10")); //106
+        System.out.println(new D100Roll().d100FromRoll("96", "99", "10", "10")); //205
+        System.out.println(new D100Roll().d100FromRoll("96", "99", "97", "10")); //302
+        System.out.println(new D100Roll().d100FromRoll("5", "10", "10", "10")); //-5
+        System.out.println(new D100Roll().d100FromRoll("5", "97", "10", "10")); //-102
+        System.out.println(new D100Roll().d100FromRoll("5", "97", "98", "10")); //-200
+        System.out.println(new D100Roll().d100FromRoll("5", "15", "98", "10")); //-10
+        System.out.println(new D100Roll().d100FromRoll("96", "15", "98", "10")); //111
     }
+
 
 }
