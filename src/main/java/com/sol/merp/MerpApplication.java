ec1f5f56993a77c22f579d9cfcd03821d585af08
Peter Kovacs <peter.kovacs@dpc.hu>
2019-06-18 09:53:26 +0200

    Initial commit

---
+++ src/main/java/com/sol/merp/MerpApplication.java
@@ -0,0 +1,13 @@
+package com.sol.merp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerpApplication.class, args);
+    }
+
+}
